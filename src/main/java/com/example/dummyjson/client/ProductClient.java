6188dccdf2b7d89acd9c676fa024e10ead38217f
LeonardoSlonzo <leonardoslonzo@hotmail.com>
2025-02-19 11:42:51 -0300

    Removendo RestTemplate e colocando OpenFeign

---
+++ src/main/java/com/example/dummyjson/client/ProductClient.java
@@ -0,0 +1,22 @@
+package com.example.dummyjson.client;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.dummyjson.config.FooConfiguration;
+import com.example.dummyjson.dto.Product;
+
+@FeignClient(name = "Dummy", url = "https://dummyjson.com/", configuration = FooConfiguration.class)
+public interface ProductClient {
+	
+	@RequestMapping(method = RequestMethod.GET, value = "products/")
+	List<Product> getAllProduct();
+	
+	@RequestMapping(method = RequestMethod.GET, value = "products/{id}")
+	Product getProductById(@PathVariable Long id);
+	
+}
---
+++ src/main/java/com/example/dummyjson/config/FooConfiguration.java
@@ -0,0 +1,22 @@
+package com.example.dummyjson.config;
+
+import org.springframework.cloud.openfeign.clientconfig.FeignClientConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import feign.Contract;
+
+@Configuration
+public class FooConfiguration {
+    @Bean
+    public FeignClientConfigurer feignClientConfigurer() {
+        return new FeignClientConfigurer() {
+            @Override
+            public boolean inheritParentConfiguration() {
+                 return false;
+            }
+        };
+    }
+}
+
+ 
